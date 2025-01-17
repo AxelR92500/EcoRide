8eb3df6d6d493115157d8df55606139c79e80b1a
AxelR92500 <axel.rajiv16@gmail.com>
2025-01-18 00:22:26 +0100

    Ajout de convoit.html

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const afficheMenu = () => {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+
+const fermerMenu = () => {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
